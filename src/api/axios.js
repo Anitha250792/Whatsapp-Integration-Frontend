83d9c61c9e993f3960060ed56c6c0b3b54bdc05d
Anitha250792 <ntanithasaravanan@gmail.com>
2026-01-17 16:51:43 +0530

    Api

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://whatsapp-integration-u7tq.onrender.com/api/",
+  baseURL: "https://whatsapp-integration-u7tq.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
